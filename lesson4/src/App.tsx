3e174d82c813d83ae5a721c7c0652654875997f7
Oleg Korneev <olvikorneev@gmail.com>
2021-10-12 13:48:35 +0300

    initial

---
+++ lesson4/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import './main.global.css';
+import { hot } from "react-hot-loader/root";
+import {Header} from './shared/Header';
+import { Content } from './shared/Content';
+import { Layout } from "./shared/Layout";
+import { CardsList } from "./shared/CardsList";
+
+function AppComponent() {
+  return (
+    <Layout>
+      <Header />
+      <Content>
+        <CardsList />
+      </Content>
+    </Layout>
+  );
+};
+
+export const App = hot(AppComponent); // это для HMR
