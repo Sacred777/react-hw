e98b7d725c2776fce85dcdfd378a940a239eb470
Oleg Korneev <olvikorneev@gmail.com>
2021-10-28 11:18:55 +0300

    add icons component to MenuItemsList.tsx

--- lesson6/src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
+++ lesson6/src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { EColors, Text } from '../../../../Text';
 import styles from './menuitemslist.css';
-import { BlockIcon, WarningIcon } from '../../../../Icons';
+import { BlockIcon, WarningIcon, CommentIcon, ShareIcon, SaveIcon } from '../../../../Icons';
 
 interface IMenuItemsListProps {
   postId: string;
@@ -11,11 +11,30 @@ export function MenuItemsList({ postId }: IMenuItemsListProps) {
   return (
     <ul className={styles.menuItemsList}>
       <li className={styles.menuItem} onClick={() => console.log(postId)}>
+        <CommentIcon />
+        <Text size={12} color={EColors.grey99}>Комментарии</Text>
+      </li>
+
+      <div className={styles.divider}></div>
+
+      <li className={styles.menuItem} onClick={() => console.log(postId)}>
+        <ShareIcon />
+        <Text size={12} color={EColors.grey99}>Поделиться</Text>
+      </li>
+
+      <div className={styles.divider}></div>
+
+      <li className={styles.menuItem} onClick={() => console.log(postId)}>
         <BlockIcon />
         <Text size={12} color={EColors.grey99}>Скрыть</Text>
       </li>
 
       <div className={styles.divider}></div>
+
+      <li className={styles.menuItem} onClick={() => console.log(postId)}>
+        <SaveIcon />
+        <Text size={12} color={EColors.grey99}>Сохранить</Text>
+      </li>
 
       <li className={styles.menuItem} onClick={() => console.log(postId)}>
         <WarningIcon />
