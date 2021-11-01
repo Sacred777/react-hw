c4ecb3fd80d5946b1d388462732c1ce5eaadf4c8
Oleg Korneev <olvikorneev@gmail.com>
2021-11-01 15:47:52 +0300

    Made the transfer styles to svg incons

--- lesson6/src/shared/Icon/Icon.tsx
+++ lesson6/src/shared/Icon/Icon.tsx
@@ -1,38 +1,53 @@
 import React from 'react';
 import styles from './icon.css';
 import classNames from 'classnames';
-import * as icons from '../../shared/Icons';
+import { MenuIcon, BlockIcon, CommentIcon, SaveIcon, ShareIcon, WarningIcon } from '../../shared/Icons';
 
-export enum EIcons {
-  block   = 'BlockIcon',
-  comment = 'CommentIcon',
-  save    = 'SaveIcon',
-  share   = 'ShareIcon',
-  warning = 'WarningIcon',
+const icons = {
+  menu:    MenuIcon,
+  block:   BlockIcon,
+  comment: CommentIcon,
+  save:    SaveIcon,
+  share:   ShareIcon,
+  warning: WarningIcon,
 }
 
-type ISizes = 12 | 14 | 16;
+export enum EIcons {
+  menu    = 'menu',
+  block   = 'block',
+  comment = 'comment',
+  save    = 'save',
+  share   = 'share',
+  warning = 'warning',
+}
+
+type TSizes = 20 | 16 | 14 | 12| 10;
 
 interface IIconProps {
   name: EIcons;
-  size?: ISizes;
+  size?: TSizes;
+  mobileSize?: TSizes;
+  tabletSize?: TSizes;
+  desktopSize?: TSizes;
 }
 
 export function Icon( props: IIconProps ) {
   const {
     name,
     size = 16,
+    mobileSize,
+    tabletSize,
+    desktopSize
   } = props;
 
   const classes = classNames(
     styles[`i${size}`],
-  )
-
-  const IconComponent = icons[name];
+    { [styles[`m${mobileSize}`]]: mobileSize},
+    { [styles[`t${tabletSize}`]]: tabletSize},
+    { [styles[`d${desktopSize}`]]: desktopSize},
+  );
 
   return (
-    <span  className={ classes }>
-      <IconComponent />
-    </span>
+    icons[name]( {className: classes} )
   );
 }
