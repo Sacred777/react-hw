66b490c239fe4e0891e7e78c9bb99261d1887e05
Oleg Korneev <olvikorneev@gmail.com>
2021-11-03 09:31:24 +0300

    Module 8 add context

--- lesson8/src/App.tsx
+++ lesson8/src/App.tsx
@@ -1,22 +1,28 @@
 import React from "react";
 import './main.global.css';
 import { hot } from "react-hot-loader/root";
-import {Header} from './shared/Header';
+import { Header } from './shared/Header';
 import { Content } from './shared/Content';
 import { Layout } from "./shared/Layout";
 import { CardsList } from "./shared/CardsList";
 import { useToken } from "./hooks/useToken";
+import { tokenContext } from './shared/context/tokenContext'
+import { UserContextProvider } from './shared/context/userContext'
 
 function AppComponent() {
   const [token] = useToken();
 
   return (
-    <Layout>
-      <Header token={token}/>
-      <Content>
-        <CardsList />
-      </Content>
-    </Layout>
+    <tokenContext.Provider value={token}>
+      <UserContextProvider>
+      <Layout>
+        <Header />
+        <Content>
+          <CardsList />
+        </Content>
+      </Layout>
+      </UserContextProvider>
+    </tokenContext.Provider>
   );
 };
 
